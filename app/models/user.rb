25640c5a0606700e7070efe9ef686d84d5462d7c
Charles Onguti <charles.onguti@student.moringaschool.com>
2024-12-12 13:09:16 +0300

    added posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     validates_uniqueness_of :email, :username
-    
+    has_many :posts
     def as_json(opts = {})
         # go to the previouis defined
         super(opts.merge(only:[:first_name, :last_name, :id, :username])) 
